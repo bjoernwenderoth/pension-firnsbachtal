48f5c788918326b70c97ec31bd3a9aafbdef64e4
Björn Wenderoth <bjoernwe1989@googlemail.com>
2026-01-08 18:46:16 +0100

    add images landing page

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,8 +1,9 @@
 export function path(slug: string): string {
   const cleanSlug = slug.startsWith("/") ? slug.slice(1) : slug;
   const base = import.meta.env.BASE_URL;
-  if (cleanSlug === "" && base.endsWith("/")) {
-    return base;
+
+  if (base === "/") {
+    return slug;
   }
   return `${base}/${cleanSlug}`;
 }
